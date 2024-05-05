3fdd93e47a50be2aa275c653737eee7b930968d5
SHASHI-17 <hibye31583@gmail.com>
2024-05-06 03:41:05 +0530

    attachments deletion complete

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -7,12 +7,12 @@ import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { Attachment, Course } from "@prisma/client";
-import { PlusCircle } from "lucide-react";
+import { File, Loader2, PlusCircle, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 interface AttachmentsProps {
-  intialData: Course & {attachments:Attachment[]};
+  intialData: Course & { attachments: Attachment[] };
   courseId: string;
 }
 
@@ -24,6 +24,8 @@ const Attachments = ({ intialData, courseId }: AttachmentsProps) => {
   const router = useRouter();
   const { toast } = useToast();
 
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+
   const [isEditing, setIsEditing] = useState(false);
   const toggleEdit = () => setIsEditing((prev) => !prev);
 
@@ -31,7 +33,7 @@ const Attachments = ({ intialData, courseId }: AttachmentsProps) => {
     try {
       await axios.post(`/api/courses/${courseId}/attachments`, values);
       toast({
-        title: "Course updated",
+        title: "Attachment Deleted",
       });
       toggleEdit();
       router.refresh();
@@ -41,6 +43,28 @@ const Attachments = ({ intialData, courseId }: AttachmentsProps) => {
         description: "There was a problem with your request.",
         variant: "destructive",
       });
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+
+      toast({
+        title: "Course updated",
+        variant: "destructive",
+      });
+      router.refresh();
+    } catch {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        variant: "destructive",
+      });
+    } finally {
+      setDeletingId(null);
     }
   };
 
@@ -58,16 +82,48 @@ const Attachments = ({ intialData, courseId }: AttachmentsProps) => {
           )}
         </Button>
       </div>
-      {!isEditing &&
+      {!isEditing && (
         <>
-            {intialData.attachments.length === 0 && (
-                <p className="text-sm mt-2 text-slate-500 italic">No attachments yet</p>
-            )}
-        </>}
+          {intialData.attachments.length === 0 && (
+            <p className="text-sm mt-2 text-slate-500 italic">
+              No attachments yet
+            </p>
+          )}
+          {intialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {intialData.attachments.map((attachment) => {
+                return (
+                  <div
+                    key={attachment.id}
+                    className="flex items-center p-3 w-full bg-sky-100
+                                 border-sky-200 border text-sky-700 rounded-md"
+                  >
+                    <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                    <p className="text-sm line-clamp-1">{attachment.name}</p>
+                    {deletingId === attachment.id && (
+                      <div>
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      </div>
+                    )}
+                    {deletingId !== attachment.id && (
+                      <Button onClick={()=>onDelete(attachment.id)}
+                        variant={"ghost"}
+                        className="ml-auto hover:opacity-75 transition"
+                      >
+                        <X className="h-4 w-4 " />
+                      </Button>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </>
+      )}
       {isEditing && (
         <div>
           <FileUpload
-            endpoint='courseAttachment'
+            endpoint="courseAttachment"
             onChange={(url) => {
               if (url) {
                 onSubmit({ url: url });
@@ -75,7 +131,7 @@ const Attachments = ({ intialData, courseId }: AttachmentsProps) => {
             }}
           />
           <div className="text-sm  text-muted-foreground mt-1">
-           Add anything to your students might need to complete the course.
+            Add anything to your students might need to complete the course.
           </div>
         </div>
       )}
