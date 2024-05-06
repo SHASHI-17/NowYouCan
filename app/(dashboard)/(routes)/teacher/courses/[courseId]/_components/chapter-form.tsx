b763ab21f2feedb1424485ea0d504ab293965410
SHASHI-17 <hibye31583@gmail.com>
2024-05-06 18:45:48 +0530

    chapter title and updation d9

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
@@ -13,15 +13,14 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
 import { Chapter, Course } from "@prisma/client";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import {ChaptersList} from "./chapter-list";
+import { ChaptersList } from "./chapter-list";
 
 interface ChapterFormProps {
   intialData: Course & {chapters:Chapter[]};
@@ -67,8 +66,38 @@ const ChapterForm = ({ intialData, courseId }: ChapterFormProps) => {
     }
   };
 
+  const onReorder =async (updatedData:{id:string,position:number}[])=>{
+    try {
+          setIsUpdating(true);
+          await axios.put(`/api/courses/${courseId}/chapters/reorder`,{list:updatedData});
+          toast({
+            title: "Chapters reordered",
+          });
+          router.refresh();
+    } catch (e) {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        variant: "destructive",
+      });
+    }finally{
+      setIsUpdating(false);
+    }
+  }
+
+  const onEdit = (id:string)=>{
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  }
+
   return (
-    <div className="mt-3 border bg-slate-100 rounded-md p-4">
+    <div className="mt-3 relative border bg-slate-100 rounded-md p-4">
+      {
+        isUpdating && <div className=" absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md 
+        flex items-center justify-center
+        ">
+          <Loader2 className=" animate-spin h-6 w-6 text-sky-700" />
+          </div>
+      }
       <div className="font-medium flex items-center justify-between">
         Course Description
         <Button variant="ghost" onClick={toggleCreating}>
@@ -114,7 +143,7 @@ const ChapterForm = ({ intialData, courseId }: ChapterFormProps) => {
         !isCreating && (
           <div className={cn("text-sm mt-2",!intialData?.chapters.length && "text-slate-500 italic")}>
            {!intialData.chapters.length && 'No chapters'}
-          <ChaptersList onEdit={()=>{}} onReorder={()=>{}} items={intialData.chapters || []} />
+          <ChaptersList onEdit={onEdit} onReorder={onReorder} items={intialData.chapters || []} />
           </div>
         )
       }
